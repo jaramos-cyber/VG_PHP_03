3c75f64133e8a2f839c0118b94d8930e2a16113a
jaramos-cyber <j.a.ramos@uni9.edu.br>
2026-03-19 14:31:42 +0000

    19/03/26

---
+++ src/recebe.php
@@ -0,0 +1,9 @@
+<h1>Recebe</h1>
+<?php 
+//echo "<pre>";print_r($_POST);echo "</pre>";
+$login=$_POST["login"];//variável login recebe tudo que foi registrado no form 
+$senha=$_POST["senha"];
+
+echo "Login: <b>$login</b><br>";
+echo "Senha: <b>$senha</b><br>"
+?>
