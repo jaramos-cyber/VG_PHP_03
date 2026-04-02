a00d8f70cea88a49577a1f81a1a0a4a481e9ae40
jaramos-cyber <j.a.ramos@uni9.edu.br>
2026-04-02 14:02:47 +0000

    02/04/26

---
+++ src/topico042.php
@@ -0,0 +1,27 @@
+<?php
+$a="10";
+$b="11";
+echo "Igualdade = ".($a==$b)."<br>";
+echo "Idêntico = ".($a===$b)."<br>";
+echo "Não igual = ".($a!=$b)."<br>";
+echo "Não Idêntico = ".($a!==$b)."<br>";
+echo "<hr>";
+$a=10;
+$b=20;
+$c=50;
+$d=($a<=$b);
+var_dump($d);
+echo "<hr>";
+$e=($a<=$c) && !($c>1000);
+var_dump($e);
+echo "<hr>";
+$f=!($a>$b) || ($c>1000);
+var_dump($f);
+echo "<hr> xor=$a, $b,$c ---";
+$g= (!($a<$b) xor ($b<$c));
+var_dump($g);
+echo "<hr>";
+$d=($a<=$b) ? "Verdadeiro" : "Falso";
+$e=($a>=$c) ? "Verdadeiro" : "Falso";
+echo "d = $d<br>e = $e";
+?>
